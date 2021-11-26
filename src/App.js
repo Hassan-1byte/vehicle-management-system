2f3046049dd0ced98b723cacff73cfb868551cac
Hassan-1byte <sh.hassan.qayyum@gmail.com>
2021-11-25 16:55:29 -0800

    new-build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Homepage from "./pages/Homepage";
 import Navbar from "./components/Navbar";
-
 import "../src/scss/style.scss";
 import Sidebar from "./components/Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
